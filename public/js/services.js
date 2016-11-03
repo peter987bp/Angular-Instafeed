53501ebb4bf11282e213b2406b2d7fbc28d2cd21
peter987bp <peter987bp@gmail.com>
2016-11-03 10:14:31 -1000

    Delete services.js

--- public/js/services.js
+++
@@ -1,9 +0,0 @@
-angular.module('myApp')
-.service('PictureService', ['$http', function($http){
-  this.fetch = () => {
-
-    //HIDE THIS TOKEN
-    return $http.jsonp('https://api.instagram.com/v1/users/1369016149/media/recent/?count=99&&callback=JSON_CALLBACK&access_token=1369016149.afe9a65.1b6ef1da7d8c4ea4abe76c450623bf13');
-  };
-}]);
-
